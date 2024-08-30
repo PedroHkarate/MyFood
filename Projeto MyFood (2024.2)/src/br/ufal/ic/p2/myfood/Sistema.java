59ffce0a430b281fbf4102be88d68518f6622de7
bruno <bhsr@ic.ufal.br>
2024-08-30 00:09:28 -0300

    updated

--- Projeto MyFood (2024.2)/src/br/ufal/ic/p2/myfood/Sistema.java
+++ Projeto MyFood (2024.2)/src/br/ufal/ic/p2/myfood/Sistema.java
@@ -9,8 +9,6 @@ public class Sistema {
         super(nome, email, senha, endereco);
     }*/
 
-    //conferir se as linhas 14-23 estão certas
-
     private final Usuario usuario;
     private ArrayList<Usuario> usuarios;
     private ArrayList<Restaurante> restaurantes;
@@ -32,6 +30,48 @@ public class Sistema {
         secoesAtivas.clear();
     }
 
+    // possivel solução pra não usar parametros estaticos (36-74), as funções originais estão comentadas em baixo
+
+    public String criarUsuario(String nome, String email, String senha, String endereco)
+            throws NomeInvalidoException, EmailInvalidoException,
+            SenhaInvalidaException, EnderecoInvalidoException {
+
+        if (nome == null) throw new NomeInvalidoException();
+        if (email == null) throw new EmailInvalidoException();
+        if (senha == null) throw new SenhaInvalidaException();
+        if (endereco == null) throw new EnderecoInvalidoException();
+
+        Usuario usuario = new Usuario(usuarios.size() + 1, nome, email, senha, endereco);
+
+        boolean adicionado = usuarios.add(usuario);
+        return nome;
+    }
+
+    public String getAtributoUsuario(int id, String atributo) throws UsuarioNaoCadastradoException {
+        Usuario usuario = null;
+        for (Usuario u : usuarios) {
+            if (u.getId() == id) {
+                usuario = u;
+                break;
+            }
+        }
+        if (usuario == null) {
+            throw new UsuarioNaoCadastradoException();
+        }
+        if (atributo.equalsIgnoreCase("nome")) {
+            return usuario.getNome();
+        } else if (atributo.equalsIgnoreCase("email")) {
+            return usuario.getEmail();
+        } else if (atributo.equalsIgnoreCase("senha")) {
+            return usuario.getSenha();
+        } else if (atributo.equalsIgnoreCase("endereco")) {
+            return usuario.getEndereco();
+        } else {
+            throw new UsuarioNaoCadastradoException();
+        }
+    }
+
+/*
     public String criarUsuario(String nome, String email, String senha, String endereco) throws NomeInvalidoException, EmailInvalidoException,
             SenhaInvalidaException, EnderecoInvalidoException{
         int id = Usuario.getId();
@@ -81,8 +121,11 @@ public class Sistema {
         }
         else throw new UsuarioNaoCadastradoException();
     }
+*/
 
-    /*public void criarUsuario(String nome, String email, String senha, String endereco, String cpf) throws NomeInvalidoException, EmailInvalidoException,
+    /*
+
+    public void criarUsuario(String nome, String email, String senha, String endereco, String cpf) throws NomeInvalidoException, EmailInvalidoException,
             SenhaInvalidaException, EnderecoInvalidoException, CpfInvalidoException{
         if(!usuarios.contains(nome)){
             if(nome == null) throw new NomeInvalidoException();
--- Projeto MyFood (2024.2)/src/br/ufal/ic/p2/myfood/Usuario.java
+++ Projeto MyFood (2024.2)/src/br/ufal/ic/p2/myfood/Usuario.java
@@ -1,5 +1,47 @@
 package br.ufal.ic.p2.myfood;
 
+public class Usuario {
+    private int id;
+    private String nome;
+    private String email;
+    private String senha;
+    private String endereco;
+
+    public Usuario(int id, String nome, String email, String senha, String endereco){
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.endereco = endereco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+}
+
+/*
+classe original estatica:::
+
+package br.ufal.ic.p2.myfood;
+
 // id, nome, email, senha e endereço NÃO deveriam ser estaticos(?) ja que são atributos compartilhados
 
 public class Usuario {
@@ -37,3 +79,5 @@ public class Usuario {
         return endereco;
     }
 }
+
+ */
